583d21f5f7f8546860f063f0bdbc41dd34c4537c
18010913593 <1061505447@qq.com>
2020-11-29 15:14:20 +0800

    引入element-ui

--- src/components/elementUi01.vue
+++ src/components/elementUi01.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <el-button @click="show = !show">Click Me</el-button>
+    <el-button @click="show = !show">点我</el-button>
     <div style="display: flex; margin-top: 20px; height: 100px;">
       <transition name="el-fade-in-linear">
         <div v-show="show" class="transition-box">.el-fade-in-linear</div>
